0237a57e252203f44629e4817c6b4674e3258c58
mischatch <mikhail@a-d-o.com>
2020-11-06 13:28:08 -0500

    deploy fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const TopNav = () => {
   return (
     <div className="footer">
-      
+
     </div>
   )
 }
